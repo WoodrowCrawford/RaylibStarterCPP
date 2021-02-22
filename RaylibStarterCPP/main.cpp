6073f401d242ce49df5ef76964d15d229f5964c2
WoodrowCrawford <woodrowcrawford03@gmail.com>
2021-02-22 12:21:42 -0600

    Added personal hash function

--- RaylibStarterCPP/main.cpp
+++ RaylibStarterCPP/main.cpp
@@ -20,6 +20,44 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include <iostream>
+
+unsigned int LodisHash(unsigned char* data)
+{
+    unsigned int hash = 0;
+    unsigned int x = 0;
+
+    for (unsigned char* i = data; *i != '\0'; ++i)
+    {
+        hash = (hash << 4) + *i;
+        if ((x = hash & 0xF0000000L) != 0)
+        {
+            hash ^= (x >> 4);
+            hash &= ~x;
+        }
+    }
+
+    return (hash & 0x7FFFFFFF);
+}
+
+
+unsigned int MyHashBrown(unsigned char* data)
+{
+    unsigned int hashBrown = rand() %  1100000000000 * (rand() % 1000000000000);
+    unsigned int x = rand() % 100000000;
+
+    for (unsigned char* i = data; *i != '\0'; ++i)
+    {
+        hashBrown = (hashBrown << 4) + *i;
+        if ((x = hashBrown & 0xF0000000L) != 0)
+        {
+            hashBrown ^= (x >> 4);
+            hashBrown &= ~x;
+        }
+    }
+
+    return (hashBrown & 0x7FFFFFFF);
+}
 
 int main(int argc, char* argv[])
 {
@@ -33,21 +71,27 @@ int main(int argc, char* argv[])
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
-
+    int checkSum = 0;
+    unsigned char* input = new unsigned char[0];
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+       
+        std::cin >> input;
+        checkSum = MyHashBrown(input);
+
+
         //----------------------------------------------------------------------------------
 
+        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(GetColor(checkSum));
 
         EndDrawing();
         //----------------------------------------------------------------------------------
